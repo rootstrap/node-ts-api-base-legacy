e0d8e14ab058371ed91514a4a991dc04e78e6421
Mauricio Cousillas <mauricio.cousillas@rootstrap.com>
2019-06-28 11:30:26 -0300

    Code improvements

--- src/mail/EmailService.ts
+++ src/mail/EmailService.ts
@@ -1,19 +1,15 @@
-import nodemailer, { Transporter, SendMailOptions } from 'nodemailer';
+import nodemailer, { SendMailOptions } from 'nodemailer';
 
 export class EmailService {
-  private transporter: Transporter;
-
-  constructor() {
-    this.transporter = nodemailer.createTransport({
-      service: process.env.SMTP_SERVICE,
-      secure: true,
-      pool: true,
-      auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS,
-      },
-    });
-  }
+  private transporter = nodemailer.createTransport({
+    service: process.env.SMTP_SERVICE,
+    secure: true,
+    pool: true,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
+    },
+  });
 
   public async send(options: SendMailOptions) {
     return this.transporter.sendMail(options);
